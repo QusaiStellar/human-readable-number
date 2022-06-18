c700f3de40c2e503c696cb3c49a2fb14896328d7
Andrew Dobrik <omgdobrik@gmail.com>
2022-06-19 00:32:26 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+   let result = "";
+   let numbers = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+
+   switch (number.toString().length) {
+      case 1:
+         result = numbers[number];
+         console.log(result);
+
+         break;
+      case 2:
+         result = `${numbers[number.toString()[0]]}ty ${numbers[number.toString()[1]]}`;
+         console.log(result);
+
+         break;
+
+      default:
+         break;
+   }
+
+   return result;
 }
+
+const myf = (number) => {
+   let result = "";
+   const numberArr = number.toString().split("");
+   const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen",];
+
+
+
+   switch (numberArr.length) {
+      case 3:
+         result = `${numbers[numberArr[0]]} hundred `;
+         console.log(result);
+      case 2:
+         let tenth;
+
+         numberArr.length > 2 ?
+            tenth = +numberArr.slice(1).join("") :
+            tenth = number;
+
+         let tenthArr = tenth.toString().split("");
+
+         switch (tenth) {
+            case 11:
+            case 12:
+            case 13:
+               result = result + numbers[tenth];
+               console.log(result);
+               break;
+            default:
+               result = `${result} ${numbers[tenthArr[1]]}teen `;
+               console.log(result);
+               break;
+         }
+      default:
+         result = result + numbers[tenth];
+         break;
+   }
+
+   return result;
+}
+
+myf(242)
